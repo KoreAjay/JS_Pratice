fd3635aae71aa915aaa96b7b1eabf1a7efd15327
Ajay Kore <160569420+KoreAjay@users.noreply.github.com>
2026-02-19 16:37:57 +0000

    container config

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+console.log("Welcome to JS");
+
+let arr=["A","J","A","Y"];
+
+for(let i=0;i<=arr.length;i++)
+{
+    console.log(arr[i]);
+    setTimeout(()=>{
+
+    },2000);
+}
