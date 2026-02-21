9ac072e5dd20d5a1760201fe9b32b1ceb91653c6
Ajay Kore <160569420+KoreAjay@users.noreply.github.com>
2026-02-21 14:37:45 +0000

    ConversitionOperation

---
+++ 02_conversitionOperaction.js
@@ -0,0 +1,51 @@
+// let score="33abc";
+// let score=null;
+// let score=true;
+
+
+
+// // const {score}=req.body;
+
+// console.log(typeof score );
+// console.log(typeof (score));
+// console.log(score)
+
+// let valueInNumber=Number (score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0;
+// let isLoggedIn=1;
+// let isLoggedIn=true;
+// let isLoggedIn="ajay";
+// let isLoggedIn="";//false
+
+
+
+
+// let booleanISLoggedIn=Boolean(isLoggedIn)
+
+// console.log(booleanISLoggedIn);
+
+//1=>true;0=>false;
+//""=>false;
+//"ajay"=>true;
+
+// let sumNumber=33;
+
+// let digitString=String(sumNumber);
+
+// console.log(digitString);
+// console.log(typeof digitString); //string
+
+
+
+//******************************** Operations *****************************
+
+let value=3;
+let negValue=-value;
+
+console.log(negValue); //-3
