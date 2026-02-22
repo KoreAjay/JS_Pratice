a7cae78c988954f8a9e2dc37ed7960158a3eedba
Ajay Kore <160569420+KoreAjay@users.noreply.github.com>
2026-02-22 12:19:36 +0000

    comparision

--- 02_conversitionOperaction.js
+++ 02_conversitionOperaction.js
@@ -45,7 +45,22 @@
 
 //******************************** Operations *****************************
 
-let value=3;
-let negValue=-value;
+// let value=3;
+// let negValue=-value;
 
-console.log(negValue); //-3
+// console.log(negValue); //-3
+
+let str1="hello";
+let str2="world";
+
+let fullString=str1+" "+str2;
+
+console.log(fullString); //hello world
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+"3") //13
+console.log("1"+2+3) //123
+console.log(1+"2"+3) //123
+console.log(1+2+"3") //33
+
